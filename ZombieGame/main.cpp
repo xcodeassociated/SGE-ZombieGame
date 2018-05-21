85220e57e7a9a2d480b5223d3348a0966788ce9f
xcodeassociated <janusz.majchrzak@student.uj.edu.pl>
2018-05-22 00:16:59 +0200

    Added entt example (from github) to the ZombieGame main.cpp.

--- ZombieGame/main.cpp
+++ ZombieGame/main.cpp
@@ -5,8 +5,12 @@
 //  Created by Hamashy on 13.02.2016.
 //  Copyright © 2016 Hamashy. All rights reserved.
 //
+
 #include <iostream>
 #include <boost/filesystem.hpp>
+
+#include <entt/entt.hpp>
+#include <cstdint>
 
 #include <Game/sge_game.hpp>
 #include <Game/Director/sge_director.hpp>
@@ -17,9 +21,64 @@
 
 namespace fs = boost::filesystem;
 
+/* <entt example> */
+struct Position {
+	float x;
+	float y;
+};
+
+struct Velocity {
+	float dx;
+	float dy;
+};
+
+void update(entt::DefaultRegistry &registry) {
+	auto view = registry.view<Position, Velocity>();
+
+	for (auto entity : view) {
+		// gets only the components that are going to be used ...
+
+		auto &velocity = view.get<Velocity>(entity);
+
+		velocity.dx = 0.;
+		velocity.dy = 0.;
+
+		// ...
+	}
+}
+
+void update(std::uint64_t dt, entt::DefaultRegistry &registry) {
+	registry.view<Position, Velocity>().each([dt](auto entity, auto &position, auto &velocity) {
+		// gets all the components of the view at once ...
+
+		position.x += velocity.dx * dt;
+		position.y += velocity.dy * dt;
+
+		// ...
+	});
+}
+/* </entt_example> */
+
 int main(int argc, char * argv[])
 {
     (void)argc; (void)argv;
+
+	/* <entt example> */
+	{
+		entt::DefaultRegistry registry;
+		std::uint64_t dt = 16;
+
+		for (auto i = 0; i < 10; ++i)
+		{
+			auto entity = registry.create();
+			registry.assign<Position>(entity, i * 1.f, i * 1.f);
+			if (i % 2 == 0) { registry.assign<Velocity>(entity, i * .1f, i * .1f); }
+		}
+
+		update(dt, registry);
+		update(registry);
+	}
+	/* </entt_example> */
 
     std::cout.setf(std::ios::boolalpha);
     std::cout.sync_with_stdio(true);
